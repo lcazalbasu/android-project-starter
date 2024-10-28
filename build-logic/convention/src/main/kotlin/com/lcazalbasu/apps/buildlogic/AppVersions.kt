9e2196cee7aca950650a5124f618ddfd1f7ffec6
Laurentiu Cazalbasu <>
2024-10-28 21:30:58 +0200

    Update release data

--- build-logic/convention/src/main/kotlin/com/lcazalbasu/apps/buildlogic/AppVersions.kt
+++ build-logic/convention/src/main/kotlin/com/lcazalbasu/apps/buildlogic/AppVersions.kt
@@ -21,6 +21,6 @@ object AppVersions {
     const val COMPILE_SDK = 34
     const val TARGET_SDK = 34
 
-    const val VERSION_CODE = 1
-    const val VERSION_NAME = "1.0.0"
+    const val VERSION_CODE = 2
+    const val VERSION_NAME = "1.0.1"
 }
